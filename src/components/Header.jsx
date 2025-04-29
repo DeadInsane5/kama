2756e24770f13c501a7b2092d87bc5daec636dca
DeadInsane5 <105852185+DeadInsane5@users.noreply.github.com>
2025-04-29 21:50:26 +0530

    Added homepage styleing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,13 @@ import './Header.css'
 
 const Header = () => {
   return (
-    <div className="header p-4 flex justify-between shadow-lg">
-        <h1>Velvet</h1>
-        <nav>
+    <div className="header p-4 flex justify-between align-middle shadow-lg">
+        <h1 className='title pl-2 text-2xl'>Velvet</h1>
+        <nav className='nav flex'>
             <ul className='flex gap-4'>
-                <li>Discover</li>
-                <li>Browse</li>
-                <li>Login</li>
+                <li className='p-2 text-[#741616]'>Discover</li>
+                <li className='p-2'>Browse</li>
+                <li className='login-button pt-2 pb-2 pl-4 pe-4 bg-[#741616] text-white rounded-xl'>Login</li>
             </ul>
         </nav>
     </div>
